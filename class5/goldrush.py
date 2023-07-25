40990d54d21474764f7e3191e5cb8748d6cb097f
har5h1l <28hshah@gmail.com>
2023-07-25 09:52:27 -0700

    update solution D

--- class5/goldrush.py
+++ class5/goldrush.py
@@ -1,23 +1,21 @@
 # https://codeforces.com/contest/1829/problem/D
 
-# if prime factorization of n has more 3s than it can be taken care of
-# if it has more 2s it cannot
-# if target has more 2s it can as long as the number of more 2s is lessthan or equal to the number of less 3s
-# other prime factors have to be equal
-
-def primefactorization(n):
+def primecounts(n):
     f = []
     
+    # number of 2s as factor
     i = 0
     while n % 2 == 0:
         n //= 2
         i += 1
     
+    # number of 3s as factor
     j = 0
     while n % 3 == 0:
         n //= 3
         j += 1
 
+    # other prime factors
     d = 5
     while n != 1 and d <= n:
         if n % d == 0:
@@ -31,8 +29,8 @@ for _ in range(int(input())):
     n, m = list(map(int, input().split())) # n = start; m = target
  
     if n > m:
-        f1, i1, j1 = primefactorization(n)
-        f2, i2, j2 = primefactorization(m)
+        f1, i1, j1 = primecounts(n)
+        f2, i2, j2 = primecounts(m)
 
         if i1 > i2: print('NO')
 
