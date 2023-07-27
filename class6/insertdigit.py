7f131062293c6ea302caee6f8f3f12330fafb0a8
har5h1l <28hshah@gmail.com>
2023-07-27 15:27:39 -0700

    correct insertdigit

--- class6/insertdigit.py
+++ class6/insertdigit.py
@@ -1,12 +1,11 @@
 # https://codeforces.com/contest/1811/problem/A
-
+ 
 for _ in range(int(input())):
     n, d = list(map(int, input().split()))
-    s = input().split() + ['-1']
-
+    s = list(input()) + ['-1']
+ 
     for i in range(n+1): 
-        print(i, s[i])
-        if int(s[i]) <= d: 
+        if int(s[i]) < d: 
             s.insert(i, str(d))
             break
         
