72e1d1e72dde480d6ee1972108ffd6583678336f
har5h1l <28hshah@gmail.com>
2023-07-27 13:37:42 -0700

    update A

--- class6/insertdigit.py
+++ class6/insertdigit.py
@@ -1,5 +1,12 @@
 # https://codeforces.com/contest/1811/problem/A
 
 for _ in range(int(input())):
-    n, d = list(map(int, input().split*()))
-    print(s:=input()[:(i:=sorted((s+d).split()).index(d))] + d * (n - len(s)) + s[i:])
+    n, d = input().split()
+    s = input().split()
+    x = int(n) - len(s) # number of additional digits
+
+    if x > 0:
+        for i in range(int(n)): 
+            if s[i] < d: s.insert(i, d * x)
+    
+    print(''.join(s))
