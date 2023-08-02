3e25b3957ce3a64489845bb03ba4c3a374324934
har5h1l <28hshah@gmail.com>
2023-08-01 19:52:46 -0700

    progress on wk 6. conveyor belt

--- class6/conveyorbelt.py
+++ class6/conveyorbelt.py
@@ -1,6 +1,10 @@
 # https://codeforces.com/contest/1811/problem/B
 
+def conveyor(x, y, n):
+    for i in range(1, int(n/2 + 1)):
+        if x in (i, n-i+1): return i
+        elif y in (i, n-i+1): return i
+
 for _ in range(int(input())):
     n, x1, y1, x2, y2 = list(map(int, input().split()))
-    
-    
+    print(abs(conveyor(x1, y1, n) - conveyor(x2, y2, n)))
